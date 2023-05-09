5fd3bd6dc58509c0c2a5d47d94b59aa5c7a87fcc
Emmanuel-Abreu <28583353+Emmanuel-Abreu@users.noreply.github.com>
2023-05-08 23:26:56 -0400

    modified CurrencyDropdown components initial state

--- src/components/CurrencyDropdown.js
+++ src/components/CurrencyDropdown.js
@@ -1,32 +1,26 @@
-import React, { useContext } from "react";
-import { useDispatch} from "react-redux";
-import { createStore } from "redux";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
-import { AppReducer } from "../context/AppContext";
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import '../assets/styles/CurrencyDropdown.css'
 
-const store = createStore(AppReducer);
-
 const CurrencyDropdown = () => {
-  const { currency } = useContext(AppContext);
-  const dispatch = useDispatch();
+  const { state = { currency: "£ Pound"}, dispatch } = useContext(AppContext);
 
   const changeCurrency = (chosenCurrency) => {
-
     dispatch({
       type: 'CHG_CURRENCY',
       payload: chosenCurrency
     });
+    console.log(chosenCurrency)
   }
 
   return (
-    <DropdownButton id="dropdown-basic-button" title={"Currency (" + { currency } + ")"}>
-      <Dropdown.Item onClick={changeCurrency("$ Dollar")}>$ Dollar</Dropdown.Item>
-      <Dropdown.Item onClick={changeCurrency("£ Pound")}>£ Pound</Dropdown.Item>
-      <Dropdown.Item onClick={changeCurrency("€ Euro")}>€ Euro</Dropdown.Item>
-      <Dropdown.Item onClick={changeCurrency("₹ Ruppee")}>₹ Ruppee</Dropdown.Item>
+    <DropdownButton id="dropdown-basic-button" title={"Currency (" + state.currency  + ")"}>
+      <Dropdown.Item onClick={() => changeCurrency("$ Dollar")}>$ Dollar</Dropdown.Item>
+      <Dropdown.Item onClick={() => changeCurrency("£ Pound")}>£ Pound</Dropdown.Item>
+      <Dropdown.Item onClick={() => changeCurrency("€ Euro")}>€ Euro</Dropdown.Item>
+      <Dropdown.Item onClick={() => changeCurrency("₹ Ruppee")}>₹ Ruppee</Dropdown.Item>
     </DropdownButton>
   );
 }
